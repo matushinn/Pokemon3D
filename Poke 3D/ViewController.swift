e08c143ad7c38e38442f0e84358e98c5b9ac683b
shotaro-ooe <shoooe97@gmail.com>
2021-08-12 09:11:03 +0900

    ポケモンが垂直に立つようにした

--- Poke 3D/ViewController.swift
+++ Poke 3D/ViewController.swift
@@ -22,6 +22,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
+        //クリアにみせる
+        sceneView.autoenablesDefaultLighting = true
         
     }
     
@@ -55,27 +57,40 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        
         let node = SCNNode()
-     
+        
+        if let imageAnchor = anchor as? ARImageAnchor{
+            
+            let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+            
+            plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
+            
+            
+            let planeNode = SCNNode(geometry: plane)
+            
+            planeNode.eulerAngles.x = -.pi/2
+            
+            node.addChildNode(planeNode)
+            
+            if let pokeScene = SCNScene(named: "art.snassets/eevee.scn"){
+                
+                if let pokeNode = pokeScene.rootNode.childNodes.first{
+                    
+                    pokeNode.eulerAngles.x = .pi / 2
+                    
+                    planeNode.addChildNode(pokeNode)
+                }
+                
+            }
+            
+            
+            
+        }
+        
+        
         return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
         
     }
 }
